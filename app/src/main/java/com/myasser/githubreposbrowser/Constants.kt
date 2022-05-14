cc95cc92358340bc255df31434090c20c1ddf462
Mohammed Yasser <mohammedre4a@gmail.com>
2022-05-14 18:07:41 +0200

    feat: added constants to handle keys used across the app. feat: added retrofit dependency. feat: added styles for buttons and TextInputEditText

---
+++ app/src/main/java/com/myasser/githubreposbrowser/Constants.kt
@@ -0,0 +1,12 @@
+package com.myasser.githubreposbrowser
+
+object Constants {
+    val search_by_repo=1
+    val search_by_user=2
+    val app_pref="app_sp"
+    val key_query="query_type"
+    val key_person_name="person_name"
+    val key_github_username="user"
+    val key_language="repo language"
+    val key_repo_search="repo_name"
+}
