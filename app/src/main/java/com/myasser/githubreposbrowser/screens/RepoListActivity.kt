eedc41d39e4baafb8b928d0099589ba8871e64cb
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-07 08:45:29 +0200

    feat: added UI for RepoListActivity.kt, waiting for implementing retrofit functionalities.

--- app/src/main/java/com/myasser/githubreposbrowser/screens/RepoListActivity.kt
+++ app/src/main/java/com/myasser/githubreposbrowser/screens/RepoListActivity.kt
@@ -1,12 +1,22 @@
 package com.myasser.githubreposbrowser.screens
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.myasser.githubreposbrowser.Adapters.RepoRecyclerViewAdapter
 import com.myasser.githubreposbrowser.R
 
 class RepoListActivity : AppCompatActivity() {
+    private lateinit var repoRecyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_repo_list)
+        findViewById<TextView>(R.id.nameTextView).text = intent.getStringExtra("name")
+        findViewById<TextView>(R.id.repoNumbersTextView).text =
+            getString(R.string.no_repositories, intent.getStringExtra("repo numbers"))
+        repoRecyclerView = findViewById(R.id.repoRecyclerView)
+        repoRecyclerView.adapter = RepoRecyclerViewAdapter(arrayListOf())
+        //todo: fetch/access repo's list from search screen
     }
 }
