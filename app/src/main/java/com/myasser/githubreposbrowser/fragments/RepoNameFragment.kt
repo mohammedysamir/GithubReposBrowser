86b13115c1df9d9b1552176cf69ed66b6fc90350
Mohammed Yasser <mohammedre4a@gmail.com>
2022-08-07 22:27:54 +0200

    feat: added fetching functionalities for both UserNameFragment.kt and RepoNameFragment.kt by user-inputs.

--- app/src/main/java/com/myasser/githubreposbrowser/fragments/RepoNameFragment.kt
+++ app/src/main/java/com/myasser/githubreposbrowser/fragments/RepoNameFragment.kt
@@ -1,5 +1,6 @@
 package com.myasser.githubreposbrowser.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,10 +10,24 @@ import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.myasser.githubreposbrowser.R
+import com.myasser.githubreposbrowser.models.Repository
+import com.myasser.githubreposbrowser.screens.RepoListActivity
+import com.myasser.githubreposbrowser.screens.SearchScreen
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class RepoNameFragment : Fragment(){
+class RepoNameFragment : Fragment() {
     private lateinit var repoNameEditText: EditText
     private lateinit var repoLanguageEditText: EditText
+
+    companion object {
+        lateinit var repositories: ArrayList<Repository>
+    }
+
+    init {
+        repositories = ArrayList()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,12 +35,40 @@ class RepoNameFragment : Fragment(){
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_repo_name, container, false)
-        view.findViewById<AppCompatButton>(R.id.searchByRepoName).setOnClickListener {
-            //todo: implement fetching data from API using repo name, language and then navigate to show list of repos
-            Toast.makeText(context, "Not implemented yet!", Toast.LENGTH_SHORT).show()
-        }
         repoNameEditText = view.findViewById(R.id.repoNameEditText)
         repoLanguageEditText = view.findViewById(R.id.repoLanguageEditText)
+
+        view.findViewById<AppCompatButton>(R.id.searchByRepoName).setOnClickListener {
+            //todo: test repos fetch and error messages
+            val repoCall =
+                SearchScreen.githubAPI.getRepoByNameAndLanguage(query = "${repoNameEditText.text}+language:${repoLanguageEditText.text}")
+            repoCall.enqueue(object : Callback<ArrayList<Repository>> {
+                override fun onResponse(
+                    call: Call<ArrayList<Repository>>,
+                    response: Response<ArrayList<Repository>>,
+                ) {
+                    if (response.body()?.size!! > 0) {
+                        repositories = response.body()!!
+                        startActivity(Intent(context,
+                            RepoListActivity::class.java).apply {
+                            putExtra("by user", false)
+                            putExtra("name", repoNameEditText.text.toString())
+                            putExtra("repo numbers", repositories.size)
+                        })
+                    } else {
+                        Toast.makeText(context,
+                            getString(R.string.repo_not_found_error),
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<Repository>>, t: Throwable) {
+                    Toast.makeText(context, getString(R.string.fetch_failure), Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+            })
+        }
         return view
     }
 }
--- app/src/main/java/com/myasser/githubreposbrowser/fragments/UserNameFragment.kt
+++ app/src/main/java/com/myasser/githubreposbrowser/fragments/UserNameFragment.kt
@@ -1,5 +1,6 @@
 package com.myasser.githubreposbrowser.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +10,24 @@ import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.fragment.app.Fragment
 import com.myasser.githubreposbrowser.R
+import com.myasser.githubreposbrowser.models.Repository
+import com.myasser.githubreposbrowser.screens.RepoListActivity
+import com.myasser.githubreposbrowser.screens.SearchScreen
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class UserNameFragment : Fragment(){
+class UserNameFragment : Fragment() {
     private lateinit var userNameEditText: EditText
+
+    companion object {
+        lateinit var userRepositories: ArrayList<Repository>
+    }
+
+    init {
+        userRepositories = ArrayList()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
@@ -19,9 +35,38 @@ class UserNameFragment : Fragment(){
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_user_name, container, false)
         userNameEditText = view.findViewById(R.id.userNameEditText)
-        view.findViewById<AppCompatButton>(R.id.searchByUserName).setOnClickListener{
-            //todo: implement retrofit fetching repos by user name and then navigate to show list of repos
-            Toast.makeText(context, "Not implemented yet!", Toast.LENGTH_SHORT).show()
+        view.findViewById<AppCompatButton>(R.id.searchByUserName).setOnClickListener {
+            val userRepoCall =
+                SearchScreen.githubAPI.getUserRepos(ownerName = userNameEditText.text.toString())
+            userRepoCall.enqueue(object : Callback<ArrayList<Repository>> {
+                override fun onResponse(
+                    call: Call<ArrayList<Repository>>,
+                    response: Response<ArrayList<Repository>>,
+                ) {
+                    //todo: test fetching, error messages
+                    if (response.body()?.size!! > 0) {
+                        userRepositories = response.body()!!
+                        //navigate to RepoListActivity -> access list from there
+                        startActivity(Intent(context, RepoListActivity::class.java).apply {
+                            putExtra("by user", true)
+                            putExtra("name", userNameEditText.text.toString())
+                            putExtra("repo numbers", RepoNameFragment.repositories.size)
+                        })
+                    } else {
+                        //notify user that user's name is not found
+                        Toast.makeText(context,
+                            getString(R.string.user_not_found_error),
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<ArrayList<Repository>>, t: Throwable) {
+                    Toast.makeText(context,
+                        getString(R.string.fetch_failure),
+                        Toast.LENGTH_LONG).show()
+                }
+
+            })
         }
         return view
     }
